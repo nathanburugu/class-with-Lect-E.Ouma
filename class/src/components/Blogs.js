d1dc9606b96d2b087b9e6b1f4580e6f464c5ee7f
ybnate254 <95563035+ybnate254@users.noreply.github.com>
2022-08-31 13:03:07 +0300

    kiasi after

--- class/src/components/Blogs.js
+++ class/src/components/Blogs.js
@@ -2,17 +2,22 @@ import React, {useState , useEffect } from 'react'
 const Blogs = () => {
     const[myBlogs, setMyblogs] = useState([
         {id:1, title:'React-Library', body:'N blog We will learn', author:'Nathan Burugu'},
-        {id:2, title:'React-Library', body:'N blog We will learn', author:'Nathan Burugu'},
-        {id:3, title:'React-Library', body:'N blog We will learn', author:'Nathan Burugu'},
-        {id:4, title:'React-Library', body:'N blog We will learn', author:'Nathan Burugu'}
+        {id:2, title:'Bootstrap', body:'N blog We will learn', author:'Nathan Burugu'},
+        {id:3, title:'vue javascript', body:'N blog We will learn', author:'Nathan Burugu'},
+        {id:4, title:'PHP', body:'N blog We will learn', author:'Nathan Burugu'}
     ]);
+    const[name, setName] = useState('Nate')
+    const changeName = () => {
+        setName('kamau')
+    }
     const handleDelete = (id)=>{
         const newBlogs = myBlogs.filter(blog => blog.id !== id);
         setMyblogs(newBlogs);
     }
     useEffect(()=>{
         console.log('use effect triggered');
-    },[]);
+        
+    },[name]);
   return (
         <div className="blog">
             {myBlogs.map((blog)=>(
@@ -20,6 +25,8 @@ const Blogs = () => {
                     <h3>{blog.title}</h3>
                     <p>Written by : {blog.author}</p>
                     <button onClick={()=>handleDelete(blog.id)}>Delete</button>
+                    <button onClick={changeName} >Change Name </button>
+                    <p>{name}</p>
                 </div>
             ))}
         </div>
