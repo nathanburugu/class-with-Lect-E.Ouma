af4744b2164909a32ba5d166ab473054831a2f49
ybnate254 <95563035+ybnate254@users.noreply.github.com>
2022-09-03 08:17:09 +0300

    nate

--- class/src/App.js
+++ class/src/App.js
@@ -1,11 +1,4 @@
-// import Hello from './components/Hello'
-// import Proops from './components/Proops'
-import './App.css';
-// import Welcome from './components/Welcome';
-// import Subscribe from './components/Subscribe'
-// import Use from './components/Use'
-import Table from './components/Table'
-import React, { useRef } from 'react'
+import  Header  from './components/Header';
 import Blogs from './components/Blogs';
 
 function App() {
@@ -19,16 +12,7 @@ function App() {
   // }
   return (
     <div className='meza'>
-      {/* <input ref={inputRef} type="text" id='message' name='message' />
-      <button onClick={handleClick} >logMessage</button> */}
-
-    {/* < Proops name='Nate' chain='Gold' number='8'/>
-    <Proops name='Kamau' chain='Silver'  number='12'/>
-    <Welcome name='Kamau'/>
-    <Hello   />
-    <Subscribe className='nate'/>
-    <Use/> */}
-    {/* <Table name='Nathan' name2='Burugu' address='22 Street Kimathi Street' phone='0722978025' email='nate@gmail.com'/> */}
+      <Header/>
    <Blogs/>
     </div>
   );
--- class/src/components/Header.js
+++ class/src/components/Header.js
@@ -6,6 +6,18 @@ export const Header = () => {
       <div className="logo">
         <h1>NA<span>TE</span>🔌</h1>
       </div>
+      <div className="middle">
+        <div className="one"><a href="#">Home</a></div>
+        <div className="two"><a href="#">About</a></div>
+        <div className="three"><a href="#">Projects</a></div>
+        <div className="four"><a href="#">Skills</a></div>
+        <div className="five"><a href="#">blog</a></div>
+      </div>
+      <div className="buttons">
+        <button className='in'>Sign In</button>
+        <button className='up'>Sign Up</button>
+      </div>
     </div>
   )
 }
+export default Header
